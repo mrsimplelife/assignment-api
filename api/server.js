1d18edba442feaac18aee93846fafc4d79fdacd8
Youme <mrsimplelife@naver.com>
2023-09-10 09:36:42 +0900

    🎨

--- api/server.js
+++ api/server.js
@@ -3,10 +3,9 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json'); // 여기서 db.json은 데이터베이스 파일입니다.
 const middlewares = jsonServer.defaults();
 
-const PORT = process.env.PORT || 3000;
-
 server.use(middlewares);
 server.use(router);
-server.listen(PORT, () => {
-  console.log('JSON Server is running');
-});
+
+module.exports = (req, res) => {
+  server.handle(req, res);
+};
